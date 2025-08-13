5c7dc74091fe78baf7f35aca5ac29840832f21bf
jrogb <grobbelaarjohan65@gmail.com>
2025-08-13 20:10:25 +0000

    Created form modal component, input field component and ITForm component

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -4,6 +4,8 @@ import { FaEye } from "react-icons/fa6";
 import { IoPersonAdd } from "react-icons/io5";
 import { useState } from "react";
 import Image from "next/image";
+import ItForm from "./forms/ItForm";
+
 
 const FormModal = ({ table, type, data, id }: { table: "it" | "staff" | "hoc" | "group" | "company" | "ticket" | "event" | "announcement" | "message"; type: "create" | "update" | "delete"; data?: any; id?: number }) => {
 
@@ -11,6 +13,19 @@ const FormModal = ({ table, type, data, id }: { table: "it" | "staff" | "hoc" | 
     const icon = type === "create" ? <IoPersonAdd /> : type === "update" ? <FaEye /> : <MdDeleteForever />;
 
     const [open, setOpen] = useState(false);
+
+    const DeleteForm = () => {
+      return  type === "delete" && id ? (
+        <form action="" className="p-4 flex flex-col gap-4">
+            <span className="text-center font-medium">Are you sure you wish to delete this {table}?</span>
+            <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
+        </form>
+      ) : (
+        <ItForm type="create"/>
+      );
+    };
+
+
     return (
         <>
             <button className={`${size} flex items-center justify-center rounded-full text-white bg-afrired`}
@@ -22,7 +37,7 @@ const FormModal = ({ table, type, data, id }: { table: "it" | "staff" | "hoc" | 
 
                 <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
                     <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
-                        <h1>Hello</h1>
+                        <DeleteForm/>
                         <div className="absolute top-4 right-4 cursor-pointer">
                             <Image src="/close.png" alt="" width={14} height={14} onClick={()=> setOpen(false)}/>
                         </div>
---
+++ src/components/forms/ItForm.tsx
@@ -0,0 +1,113 @@
+"use client"
+import { Form } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import InputField from "../InputField";
+import { IoCloudUploadOutline } from "react-icons/io5";
+
+const schema = z.object({
+    username: z
+        .string()
+        .min(3, { message: "Username must be at least 3 characters" })
+        .max(30, { message: "Username must be at most 30 characters" })
+        .regex(/^[a-zA-Z0-9_]+$/, { message: "Username can only contain letters, numbers, and underscores" }),
+
+    email: z
+        .string()
+        .min(5, { message: "Email is too short" })
+        .max(100, { message: "Email is too long" })
+        .email({ message: "Invalid email format" }),
+
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters" })
+        .max(20, { message: "Password cannot be longer than 20 characters" })
+        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+        .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+        .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character" }),
+
+    firstName: z
+        .string()
+        .min(1, { message: "First name is required" })
+        .max(50, { message: "First name must be at most 50 characters" })
+        .regex(/^[a-zA-Z]+$/, { message: "First name can only contain letters" }),
+
+    lastName: z
+        .string()
+        .min(1, { message: "Last name is required" })
+        .max(50, { message: "Last name must be at most 50 characters" })
+        .regex(/^[a-zA-Z]+$/, { message: "Last name can only contain letters" }),
+
+    phone: z
+        .string()
+        .min(10, { message: "Phone number must be at least 10 digits" })
+        .max(15, { message: "Phone number must be at most 15 digits" })
+        .regex(/^\d+$/, { message: "Phone number can only contain digits" }),
+
+    birthday: z
+        .string()
+        .refine((date) => {
+            const parsedDate = new Date(date);
+            return !isNaN(parsedDate.getTime()) && parsedDate <= new Date();
+        }, { message: "Invalid date or date is in the future" }),
+
+    img: z
+        .instanceof(File, { message: "Image is required" }),
+
+});
+
+type Inputs = z.infer<typeof schema>;
+
+const ItForm = ({
+    type,
+    data,
+}: {
+    type: "create" | "update";
+    data?: any
+}) => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>({
+        resolver: zodResolver(schema),
+    });
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+    });
+
+    return (
+        <form className="flex flex-col gap-8" onSubmit={onSubmit}>
+            <h1 className="text-bold">Add a new IT member</h1>
+            <span className="text-xs text-gray-400 text-light">Account Information</span>
+            <div className="flex justify-between gap-4 flex-wrap">
+                <InputField  label="Username" name="username" defaultValue={data?.username} register={register} error={errors?.username}/>
+                <InputField  label="Email" name="email" type="email" defaultValue={data?.email} register={register} error={errors?.email}/>
+                <InputField  label="Password" name="password" type="password" defaultValue={data?.password} register={register} error={errors?.password}/>
+            </div>
+            <span className="text-xs text-gray-400 text-light">Personal Information</span>
+            <div className="flex justify-between gap-4 flex-wrap">
+                <InputField  label="First Name" name="firstName" defaultValue={data?.firstName} register={register} error={errors?.firstName}/>
+                <InputField  label="Last Name" name="lastName" defaultValue={data?.lastName} register={register} error={errors?.lastName}/>
+                <InputField  label="Phone" name="phone" defaultValue={data?.phone} register={register} error={errors?.phone}/>
+                <InputField  label="Birthday" name="birthday" type="date" defaultValue={data?.birthday} register={register} error={errors?.birthday}/>
+                <div className="flex flex-col gap-2 w-full md:w-1/4 justify-center">
+                <label className="text-sm font-semibold text-gray-500 flex items-center gap-2 cursor-pointer" htmlFor="img">
+                    <IoCloudUploadOutline width={28} height={28}/>
+                    <span>Upload a Photo</span>
+                </label>
+                <input type="file" id="img" {...register("img")} className="hidden"/>
+                {errors?.img && <p className="text-afrired text-xs">{errors?.img?.toString()}</p>}
+            </div>
+            </div>
+            
+            <button className="bg-afrired text-white p-2 rounded-md">{type === "create" ? "Create" : "Update"}</button>
+        </form>
+    );
+};
+
+export default ItForm;
